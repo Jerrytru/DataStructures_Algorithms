db4a4436d0f2646596a13e1fb64e88d1bacd3b5a
Jerry Trujillo <trujillojerry70@yahoo.com>
2020-12-17 18:46:12 -0500

    Search-Rotated-Array

--- arrays/SearchRotatedArray.java
+++ arrays/SearchRotatedArray.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package arrays;
 
-/**
- *
- * @author yudithtrujillo
- */
+
 public class SearchRotatedArray {
     
+  public static int binarySearch(int[] arr, int key) {
+    // assuming all the keys are unique.
+    
+    int start = 0;
+    int end = arr.length-1;
+    
+    if (start > end) {
+      return -1;
+    }
+    
+    while( start <= end ){
+       int mid = start + ((end - start) / 2);
+       
+       if ( arr[start] <= arr[mid] && key <= arr[mid] && key >= arr[start] ){
+           end = mid -1;
+       } else if ( arr[end] >= arr[mid] && key >= arr[mid] && key <= arr[end]){
+           start = mid + 1;
+       } else if ( arr[start] >= arr[mid]){
+           end = mid-1;
+       } else if (arr[end] <= arr[mid] ){
+           start = mid + 1;
+       }
+    }
+
+    
+    return -1;
+  }
 }
